eb30154744731c63fe719740a19ec3ec4f8a8b0e
Ian Shehadeh <ian@shehadeh.net>
2023-11-23 07:51:17 -0500

    add ident to grammar

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -15,6 +15,7 @@ pub enum InfixOp {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Node {
     Number(i32),
+    Ident(String),
     Expr {
         lhs: Box<Node>,
         op: InfixOp,
@@ -42,6 +43,7 @@ pub fn pair_to_ast<'i>(pair: Pair<'i, Rule>) -> Node {
             Node::Expr { lhs, op, rhs }
         }
         Rule::number => Node::Number(i32::from_str_radix(pair.as_str(), 10).unwrap()),
+        Rule::ident => Node::Ident(pair.as_str().to_owned()),
         Rule::program | Rule::atomic_expr => pair_to_ast(pair.into_inner().next().unwrap()),
         Rule::infix_op | Rule::EOI => unreachable!(),
         Rule::WHITESPACE => unreachable!(),
@@ -55,20 +57,59 @@ mod test {
     use super::{ReedParser, Rule};
     use pest::Parser;
 
-    #[test]
-    fn test() {
-        let nodes: Vec<_> = ReedParser::parse(Rule::program, "1 + 1")
+    fn str_to_ast(s: &str) -> Node {
+        ReedParser::parse(Rule::program, s)
             .expect("failed to parse")
-            .take_while(|x| x.as_rule() != Rule::EOI)
+            .nth(0)
             .map(|x| pair_to_ast(x))
-            .collect();
+            .unwrap()
+    }
+
+    #[test]
+    fn simple_expr() {
         assert_eq!(
-            nodes,
-            vec![Node::Expr {
+            str_to_ast("1 + 1"),
+            Node::Expr {
                 lhs: Box::new(Node::Number(1)),
                 op: InfixOp::Add,
                 rhs: Box::new(Node::Number(1))
-            }]
+            }
+        );
+    }
+
+    #[test]
+    fn simple_expr_ident() {
+        assert_eq!(
+            str_to_ast("1 + abc"),
+            Node::Expr {
+                lhs: Box::new(Node::Number(1)),
+                op: InfixOp::Add,
+                rhs: Box::new(Node::Ident("abc".to_owned()))
+            }
+        );
+    }
+
+    #[test]
+    fn parens_expr() {
+        assert_eq!(
+            str_to_ast("(1 + abc) * (10 / (5 * _1))"),
+            Node::Expr {
+                lhs: Box::new(Node::Expr {
+                    lhs: Box::new(Node::Number(1)),
+                    op: InfixOp::Add,
+                    rhs: Box::new(Node::Ident("abc".to_owned()))
+                }),
+                op: InfixOp::Mul,
+                rhs: Box::new(Node::Expr {
+                    lhs: Box::new(Node::Number(10)),
+                    op: InfixOp::Div,
+                    rhs: Box::new(Node::Expr {
+                        lhs: Box::new(Node::Number(5)),
+                        op: InfixOp::Mul,
+                        rhs: Box::new(Node::Ident("_1".to_owned()))
+                    })
+                })
+            }
         );
     }
 }
